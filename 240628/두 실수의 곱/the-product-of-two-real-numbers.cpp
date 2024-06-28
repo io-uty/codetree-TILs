a019c33f9b3bc466e3f85a350036d4c0debccdf7
io-uty <128786114+io-uty@users.noreply.github.com>
2024-06-28 13:52:54 +0900

    Title: 두 실수의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240628/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a = 5.26;
+    double b = 8.27;
+    cout << fixed << setprecision(3) << a*b;
+    return 0;
+}
