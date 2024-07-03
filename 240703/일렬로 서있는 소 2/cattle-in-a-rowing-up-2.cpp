9cb92acd1a8917dc68ebfed67676bdc5b7cc1fe1
io-uty <128786114+io-uty@users.noreply.github.com>
2024-07-03 14:05:12 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240703/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
+++ 240703/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -14,7 +14,7 @@ int main() {
     for(int i=1; i<=n; i++){
         for(int j=i+1; j<=n ;j++){
             for(int k=j+1; k<=n; k++){
-                if(arr[i]<=arr[j] && arr[j] < arr[k]){
+                if(arr[i]<=arr[j] && arr[j] <= arr[k]){
                     count++;
                 }
             }
